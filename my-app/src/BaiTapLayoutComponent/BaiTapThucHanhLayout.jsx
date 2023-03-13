1a9ab5713762d28829b191aad00c538d6ccbe4c6
Luong Vo <diamonluong>
2023-03-13 20:34:22 +0700

    baitap1

---
+++ my-app/src/BaiTapLayoutComponent/BaiTapThucHanhLayout.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./Header";
+import Banner from "./Banner";
+import Body from "./Body";
+import Item from "./Item";
+import Footer from "./Footer";
+
+function BaiTapThucHanhLayout() {
+  return (
+    <div>
+      <Header />
+      <Body />
+      <Item />
+      <Footer />
+    </div>
+  );
+}
+
+export default BaiTapThucHanhLayout;
---
+++ my-app/src/BaiTapLayoutComponent/Item.jsx
@@ -0,0 +1,85 @@
+import React from "react";
+
+function Item() {
+  return (
+    <div>
+      <section className="pt-4">
+        <div className="container px-lg-5">
+          {/* Page Features*/}
+          <div className="row gx-lg-5">
+            <div className="col-3 mb-4">
+              <div className="card bg-light border-0 h-100">
+                <div className="card-body text-center pt-0 pt-lg-0">
+                  <div className="feature bg-secondary bg-gradient text-white-50 rounded-3 mb-4">
+                    <p className="mt-3 font-text">500 x 325</p>
+                  </div>
+                  <h2 className="fs-4">Card title</h2>
+                  <p className="mb-0">
+                    With Bootstrap 5, we've created a fresh new layout for this
+                    template!
+                  </p>
+                  <button className="btn btn-primary mt-5 mb-3">
+                    Find Out More!
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div className="col-3 mb-4">
+              <div className="card bg-light border-0 h-100">
+                <div className="card-body text-center pt-0 pt-lg-0">
+                  <div className="feature bg-secondary bg-gradient text-white-50 rounded-3 mb-4">
+                    <p className="mt-3 font-text">500 x 325</p>
+                  </div>
+                  <h2 className="fs-4">Card title</h2>
+                  <p className="mb-0">
+                    As always, Start Bootstrap has a powerful collectin of free
+                    templates.
+                  </p>
+                  <button className="btn btn-primary mt-5 mb-3">
+                    Find Out More!
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div className="col-3 mb-4">
+              <div className="card bg-light border-0 h-100">
+                <div className="card-body text-center pt-0 pt-lg-0">
+                  <div className="feature bg-secondary bg-gradient text-white-50 rounded-3 mb-4">
+                    <p className="mt-3 font-text">500 x 325</p>
+                  </div>
+                  <h2 className="fs-4">Card title</h2>
+                  <p className="mb-0">
+                    As always, Start Bootstrap has a powerful collectin of free
+                    templates.
+                  </p>
+                  <button className="btn btn-primary mt-5 mb-3">
+                    Find Out More!
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div className="col-3 mb-4">
+              <div className="card bg-light border-0 h-100">
+                <div className="card-body text-center pt-0 pt-lg-0">
+                  <div className="feature bg-secondary bg-gradient text-white-50 rounded-3 mb-4">
+                    <p className="mt-3 font-text">500 x 325</p>
+                  </div>
+                  <h2 className="fs-4">Card title</h2>
+                  <p className="mb-0">
+                    We've created some custom feature boxes using Bootstrap
+                    icons!
+                  </p>
+                  <button className="btn btn-primary mt-5 mb-3">
+                    Find Out More!
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Item;
